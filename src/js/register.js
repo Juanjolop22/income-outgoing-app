7b5a240ac721ece9f2cd668b3492c6aa5a77a809
juan jose lopez <jjlr22210@gmail.com>
2025-01-30 00:03:40 -0500

    mysql installed, server.js file for node.js, created.

--- src/js/register.js
+++ src/js/register.js
@@ -7,10 +7,10 @@ export const createRegisterForm = () =>{
 const registerFormContent = (formContainer) =>{
     const registerForm = document.createElement('form');
     registerForm.innerHTML = `
-        <h2>Rgistrate</h2>
+        <h2>Registrate</h2>
         <p class = "user">Elige un nombre de usuario.</p>
         <input class = "inputForm1" type="text" name="username" placeholder="murph567" required><br>
-        <p>Elige tu contraseña.
+        <p>Elige tu contraseña.</p>
         <input class = "inputForm2" type="password" name="password" placeholder="dante321" required><br>
         <button class = "formButton" type="submit">Crear Cuenta.</button>
       `;
